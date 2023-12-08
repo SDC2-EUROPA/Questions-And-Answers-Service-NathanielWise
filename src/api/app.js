bdd63ce90fcd54d4e8339ee058c2aa0fea4716f5
Gralayer <wisenathaniel1998@gmail.com>
2023-12-07 19:10:01 -0500

    Used SQL to ETL data into a mySQL database.

--- src/api/app.js
+++ src/api/app.js
@@ -1,15 +1,81 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
+const process.env = require('dotenv');
 
 const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 })
 
 app.json()
 
-app.get('/products/questions/:match', (req, res) => {
+let getResults = [];
 
-});
+app.get('/qa/questions/', (req, res) => {
+  // Loads questions for productId, except reported ones. Should accept a page and count.
+  // const perPage = req.body.count || 5;
+  // const page = req.body.page || 1;
+  /*
+    allResults = (
+      SELECT * FROM questions
+      LEFT JOIN answers ON questions.id = answers.question_id
+      LEFT JOIN answers_photos ON answers.id = answers_photos.answer_id
+      WHERE questions.product_id = req.body.product_id
+      AND answers.question_id = questions.id
+      AND answers_photos.answer_id = answers.id
+      AND questions.reported = 0
+      AND answers.reported = 0
+      LIMIT page * perPage, (page + 1) * perPage;
+    );
+  */
+})
+
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  // Returns answers for a given question, except reported ones
+  // const perPage = req.body.count || 5;
+  // const page = req.body.page || 5;
+  // const questionNum = req.params.question_id;
+  /*
+    SELECT * FROM answers
+    LEFT JOIN answers_photos ON answers_photos.answer_id = answers.id
+    WHERE answers.question_id = question
+    AND answers_photos.answer_id = answers.id
+    AND answers.reported = 0;
+    LIMIT page * perPage, (page + 1) * perPage;
+  */
+})
+
+app.post('/qa/questions', (req, res) => {
+  // Adds a question for the given product
+  // const body = req.body.body || '';
+  // const name = req.body.name || null;
+  // const email = req.body.email || null;
+  // const product = req.body.product_id;
+  /*
+    INSERT INTO questions (columns) VALUES (values);
+  */
+})
+
+// Answer a question
+app.post('/qa/questions/:questionId/answers', (req, res) => {
+  // Adds an answer to a given question
+  // const question = req.params.question_id;
+  // const body = req.body.body;
+  // const name = req.body.name;
+  // const email = req.body.email;
+  // const photos = req.body.photos;
+  /*
+    INSERT INTO answers (columns) VALUES (values);
+    INSERT INTO answers_photos (columns) VALUES (values);
+  */
+})
+
+// Marks a question as helpful
+app.put('/products/questions/answers/photos/:answerId', (req, res) => {
+  /*
+
+  */
+})
--- src/mysql.sql
+++ src/mysql.sql
@@ -1,5 +1,5 @@
 CREATE TABLE questions (
-  id INT unsigned NOT NULL AUTO_INCREMENT,
+  id INT unsigned NOT NULL, /* Make this auto-increment after loading data */
   product_id INT unsigned NOT NULL,
   body VARCHAR(255) NOT NULL,
   date_written BIGINT unsigned NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE questions (
 );
 
 CREATE TABLE answers (
-  id INT unsigned NOT NULL,
+  id INT unsigned NOT NULL, /* Make this auto-increment after loading data */
   question_id INT unsigned NOT NULL,
   body VARCHAR(255) NOT NULL,
   date_written BIGINT unsigned NOT NULL,
@@ -24,9 +24,35 @@ CREATE TABLE answers (
 );
 
 CREATE TABLE answers_photos (
-  id INT unsigned NOT NULL,
+  id INT unsigned NOT NULL, /* Make this auto-increment after loading data */
   answer_id INT unsigned NOT NULL,
   `url` VARCHAR(255) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (answer_id) REFERENCES answers(id)
 );
+
+CREATE INDEX idx_question_id ON answers (question_id);
+CREATE INDEX idx_answers_id ON answers_photos (answer_id);
+CREATE INDEX idx_product_id ON questions (product_id);
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\questions.csv'
+INTO TABLE questions
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\answers.csv'
+INTO TABLE answers
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\answers_photos.csv'
+INTO TABLE answers_photos
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+ALTER TABLE questions AUTO_INCREMENT=3518964;
+ALTER TABLE answers AUTO_INCREMENT=6879307;
+ALTER TABLE answers_photos AUTO_INCREMENT=2063760;
